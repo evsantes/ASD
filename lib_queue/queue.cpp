e12f146ab9ae5572315ce69f9669b03c603b1fd2
evsantes <pegankina2020@gmail.com>
2025-10-14 12:26:23 +0300

    added code to queue.h

---
+++ lib_queue/queue.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+#include "queue.h"
+using namespace std;
+
+//изменили его так, как нам надо
--- lib_queue/queue.h
+++ lib_queue/queue.h
@@ -1,6 +1,75 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include <iostream>
+template <typename T>
+class Queue {
+	T* _data; //динамический массив
+	int _size; //размер
+	int _head; //индекс начала очереди
+	int _tail; //индекс конца очереди
+	int _count; //количество?
+	//int _tail;
+public:
+	void push(const T& val);
+	void pop();
+	T head() const;
+	bool is_empty();
+	bool is_full();
+	void clear() noexcept;
+	Queue(); //объ€вление функции
+	Queue(T* data, int head, int count, int tail, int size); //конструктор инициализации
+};
+
+template<class T>
+Queue<T>::Queue(T* data, int head, int count, int tail, int size) {
+	//прописываем тело функции
+	_size = size; //переменна€ класса (поле) и локальна€ переменна€ от функции, которую мы создали внутри неЄ
+	_data = new T[_size]; //new - выделение пам€ти
+	for (int i = 0; i < count; i++) {
+		_data[i] = data[i];
+	}
+	//_head = head;
+}
+
+template<class T>
+Queue<T>::Queue() : _data(nullptr), _count(0), _head(0), _size(size), _tail(-1) {} //список инициализации
+
+//еще какие-нибудь конструкторы
+
+template<class T>
+void Queue<T>::push(const T& val) {
+	if (is_full()) {
+		throw logic_error("Queue is full!");
+	}
+	_count++;
+	_data[(++_tail) % size] = val;
+}
+
+template<class T>
+void Queue<T>::pop() {
+	if (is_empty()) {
+		throw logic_error("Queue is empty!");
+	}
+	//_data[_head] = NULL;
+	_count--;
+	head = (++head) % _size;
+}
+
+template<class T>
+bool Queue<T>::is_empty() {
+	return _count == 0; //сравниваем
+}
+
+template<class T>
+bool Queue<T>::is_full() {
+	return _count == _size;
+}
+
+template<class T>
+void Queue<T>::clear() noexcept {
+	_count = 0;
+	_tail = -1;
+	_head = 0; //присваиваем
+}
 
 #endif 
