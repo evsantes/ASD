44d75537c8025584f4a68c1940631fd5f98b4cff
evsantes <pegankina2020@gmail.com>
2025-10-15 11:23:48 +0300

    added mew filese for tests

--- tests/test_easy_example.cpp
+++ tests/test_easy_example.cpp
@@ -57,6 +57,48 @@ TEST(TestStack, can_clear_stack) {
 }
 
 
+
+TEST(TestQueue, can_push_elements) {
+	Queue<int> queue;
+	queue.push(10);
+	EXPECT_FALSE(queue.is_empty());
+	EXPECT_EQ(queue.size(), 1);
+	queue.push(20);
+	queue.push(30);
+	EXPECT_EQ(queue.size(), 3);
+}
+
+TEST(TestQueue, can_create_empty_queue) {
+	Queue<int> queue;
+	EXPECT_TRUE(queue.is_empty());
+	EXPECT_EQ(queue.size(), 0);
+}
+
+TEST(TestStack, can_pop_elements) {
+	Queue<int> queue;
+	queue.push(1);
+	queue.push(2);
+	queue.push(3);
+
+	EXPECT_EQ(queue.pop(), 3);
+	EXPECT_EQ(queue.pop(), 2);
+	EXPECT_EQ(queue.pop(), 1);
+	EXPECT_TRUE(queue.is_empty());
+}
+
+TEST(TestQueue, can_clear_queue) {
+	Queue<int> queue;
+	queue.push(1);
+	queue.push(2);
+	queue.push(3);
+
+	queue.clear();
+	EXPECT_TRUE(queue.is_empty());
+	EXPECT_EQ(queue.size(), 0);
+}
+
+
+
 //#define EPSILON 0.000001
 //
 //TEST(TestEasyExampleLib, can_div) {
