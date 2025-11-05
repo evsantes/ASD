96424bd1f301ec70dc5a4a31714726775897879a
evsantes <pegankina2020@gmail.com>
2025-11-05 12:14:59 +0300

    added realization of optimization DSU

--- lib_DSU/dsu.h
+++ lib_DSU/dsu.h
@@ -4,14 +4,13 @@
 template <class T>
 class DSU {
     int* _rank;
-    int* _parents;
+    int* _parent;
     int _size;
 public:
     DSU(int _size);
     DSU(const DSU& other);
     void unite(int, int);
     int find(int);
-    T& operator[](int);
     ~DSU();
 };
 
@@ -20,26 +19,20 @@ DSU<T>::DSU(int size) : _size(size) {
     _rank = new int[size];
     _parent = new int[size];
     for (int i = 0; i < _size; i++) {
-        _rank[i] = i;
+        _rank[i] = 0;
         _parent[i] = i;
     }
 }
 
 template<class T>
 DSU<T>::DSU(const DSU& other) :_size(other._size) {
-    _rank = new int[size];
-    _parents = new int[size];
+    _rank = new int[_size];
+    _parent = new int[_size];
     for (int i = 0; i < _size; i++) {
         _rank[i] = other._rank[i];
-        _parent[i] = other._parents[i];
+        _parent[i] = other._parent[i];
     }
 }
-
-//template<class T>
-//T& DSU<T>::operator[](int index) {
-//  return _rank[index];
-//  return _parent[index];
-//}
 
 template<class T>
 DSU<T>::~DSU() {
@@ -50,21 +43,27 @@ DSU<T>::~DSU() {
 template<class T>
 int DSU<T>::find(int x) {
     if (_parent[x] == x) { return x; }
-    else { return x = find(_parent[x]); }
+    else { return _parent[x] = find(_parent[x]); }
 }
 
 template<class T>
 void DSU<T>::unite(int x1, int x2) {
-    int rank_x1 = rank[find(x1)];
-    int rank_x2 = rank[find(x2)];
-    if (rank_x1 < rank_x2) {
+    int rank_x1 = find(x1);
+    int rank_x2 = find(x2);
+    if (_rank[rank_x1] < _rank[rank_x2]) {
         _parent[x1] = find(x2);
     }
+    else if (_rank[rank_x1] > _rank[rank_x2]){
+        _parent[x2] = find(x1);
+
+        //if (_rank[x1] = _rank[x2]) {
+        //    _rank[x1]++;
+        //}
+        //else { _parent[x2] = find(x1); }
+    }
     else {
-        if (_rank[x1] = _rank[x2]) {
-            _rank[x1]++;
-        }
-        else { _parent[x2] = find(x1); }
+        _parent[x2] = find(x1);
+        _rank[rank_x1]++;
     }
 }
 
